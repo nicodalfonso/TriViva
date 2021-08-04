b47ad22b51e953aee917290fcd0a717f40fb789b
Nico Alfonso <nicodalfonso@gmail.com>
2021-08-04 13:21:03 -0400

    Fetches random category and all questions from said category

--- main.js
+++ main.js
@@ -11,14 +11,12 @@ gameBoard.append(gameScreen)
 main.append(pageTitle, subTitle, gameBoard, button)
 
 class Game {
-  constructor() {
-    isGameOver: false;
-    category: null;
-    questions: null;
-    clickHandler: null;
-  }
+  isGameOver = false;
+  category = null;
+  questions = null;
+  clickHandler = null;
+  api = "https://www.jservice.io/api";
 
-  
   init() {
     pageTitle.innerText = "¡TriViva!"
     subTitle.innerText = "Viva la Trivia, Baby"
@@ -27,24 +25,44 @@ class Game {
     gameScreen.classList.toggle("hidden")
     button.classList.toggle("without-game-board")
     this.clickHandler = this.startGame;
-    // console.log(this.startGame)
     updateClickHandler(button, this, this.startGame);
   }
   
   // Arrow function binds `this` to the class instead of the button
-  startGame = () => {
+  startGame = async () => {
     gameBoard.classList.toggle("hidden")
     gameScreen.classList.toggle("hidden")
     button.classList.toggle("without-game-board")
     main.append("Welcome to the Jungle.")
     updateClickHandler(button, this, this.testFunction);
     gameBoard.append(button)
+    await this.handleFetchData()
   }
   
   // Arrow function binds `this` to the class instead of the button
   testFunction = () => {
-    console.log("ya got me")
-    main.removeChild(main.lastChild)
+    console.log("click")
+  }
+
+  async handleFetchData() {
+    // fetch initial random question
+    
+    this.category = await this.fetchCategory()
+    this.questions = await this.fetchQuestions(this.category.id)
+    console.log(this)
+  }
+
+  async fetchCategory(){
+    let res = await fetch(`${this.api}/random`)
+    // API returns array with single object
+    let [data] = (await res.json())
+    return data.category
+  }
+
+  async fetchQuestions(categoryID) {
+    let res = await fetch(`${this.api}/category?id=${categoryID}`)
+    let data = await res.json()
+    return data.clues
   }
   
 }
@@ -53,13 +71,14 @@ class Game {
 
 let app = new Game();
 
+
 window.addEventListener("load", () => {
   app.init();
 })
 
 const updateClickHandler = (elem, manager, newClickHandler) => {
-  console.log({ elem, manager, newClickHandler })
   elem.removeEventListener("click", manager.clickHandler)
   manager.clickHandler = newClickHandler
   elem.addEventListener("click", newClickHandler)
 }
+
